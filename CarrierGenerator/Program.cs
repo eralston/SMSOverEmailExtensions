319fbda6a964111d0ea54b647ccd69bf856dd75e
Erik Ralston <erik.ralston@gmail.com>
2014-01-21 20:21:03 -0800

    Fixed mapping of enum values to real values; updating generator and library

--- CarrierGenerator/Program.cs
+++ CarrierGenerator/Program.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.IO;
 using System.Text;
 using System.Threading.Tasks;
+using System.Text.RegularExpressions;
 
 namespace CarrierGenerator
 {
@@ -11,7 +12,8 @@ namespace CarrierGenerator
     {
         static void Main(string[] args)
         {
-            StringBuilder builder = new StringBuilder();
+            StringBuilder mappingBuilder = new StringBuilder();
+            StringBuilder enumBuilder = new StringBuilder();
 
             // File must be named "CarrierInfo.csv" and be in the same directory as the program
             string[] lines = File.ReadAllLines("Carriers.csv");
@@ -23,14 +25,21 @@ namespace CarrierGenerator
                 string nameVal = values[1];
                 string templateVal = values[2];
 
+                Regex rgx = new Regex("[^a-zA-Z0-9]");
+                enumVal = rgx.Replace(nameVal, "");
+
+                enumBuilder.Append(enumVal);
+                enumBuilder.AppendLine(",");
+
                 // Create a declaration
                 string declaration = string.Format(" {{ Carrier.{0} , new CarrierInfo(Carrier.{0}, \"{1}\", \"{2}\") }},", enumVal, nameVal, templateVal);
                 // Add it to the values%
-                builder.AppendLine(declaration);
+                mappingBuilder.AppendLine(declaration);
             }
 
             // Dump the file
-            File.WriteAllText("CarrierInfo.Loads.cs", builder.ToString());
+            File.WriteAllText("CarrierInfo.Loads.cs", mappingBuilder.ToString());
+            File.WriteAllText("CarrierEnum.cs", enumBuilder.ToString());
         }
     }
 }
